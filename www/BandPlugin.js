d4427087e284b7b8d677815f5bb2523a3332605d
elio00728 <elio00728@gmail.com>
2015-07-16 14:14:01 +0200

    Update and rename SamplePlugin.js to BandPlugin.js

---
+++ www/BandPlugin.js
@@ -0,0 +1,9 @@
+
+var BandPlugin = {
+    Sample: function (successCallback, errorCallback, title, message) {
+        var args = [title, message];
+        cordova.exec(successCallback, errorCallback, "BandPlugin", "Band", [args]);
+    }
+}
+
+module.exports = BandPlugin;
--- www/SamplePlugin.js
+++
@@ -1,9 +0,0 @@
-
-var SamplePlugin = {
-    Sample: function (successCallback, errorCallback, title, message) {
-        var args = [title, message];
-        cordova.exec(successCallback, errorCallback, "SamplePlugin", "Sample", [args]);
-    }
-}
-
-module.exports = SamplePlugin;
