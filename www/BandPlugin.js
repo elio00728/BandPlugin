fa19536907fcea8c5e4e938af9bf2e6252f87882
elio00728 <elio00728@gmail.com>
2015-07-16 14:30:38 +0200

    Update BandPlugin.js

--- www/BandPlugin.js
+++ www/BandPlugin.js
@@ -1,6 +1,6 @@
 
 var BandPlugin = {
-    Sample: function (successCallback, errorCallback, title, message) {
+    Band: function (successCallback, errorCallback, title, message) {
         var args = [title, message];
         cordova.exec(successCallback, errorCallback, "BandPlugin", "Band", [args]);
     }
