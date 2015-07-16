6ab9f9bfffbc2b945f8d0c528f456a81567c7350
elio00728 <elio00728@gmail.com>
2015-07-16 15:09:20 +0200

    Create BandPlugin.js

---
+++ www/wp8/plugins/BandPlugin.js
@@ -0,0 +1,8 @@
+var BandPlugin = {
+    Band: function (successCallback, errorCallback, title, message) {
+        var args = [title, message];
+        cordova.exec(successCallback, errorCallback, "BandPlugin", "Band", [args]);
+    }
+}
+
+module.exports = BandPlugin;
